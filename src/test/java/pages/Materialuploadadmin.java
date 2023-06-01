1d49237ade916b81b36776766f049c5030fb765b
premdev94 <premdev5014@gmail.com>
2023-06-01 13:30:22 +0530

    Latest Code Changesgit add -A!

--- src/test/java/pages/Materialuploadadmin.java
+++ src/test/java/pages/Materialuploadadmin.java
@@ -17,7 +17,7 @@ public class Materialuploadadmin extends Wrapper {
 	By display=By.xpath("//*[@aria-label='Default select example']");
 	By clicklast=By.xpath("//*[text()='Last']");
 	By clickcurriculumname=By.xpath("//table/tbody/tr[last()]/td[1]");
-	By clickdocs=By.xpath("//*[text()='Docs']");
+	By clickdocs=By.xpath("//*[text()='Ref Doc']");
 	By linkclick=By.xpath("//*[text()='+']");
 	By url1=By.id("addUrl0");
 	By url2=By.id("addUrl1");
